a4611f33a4a41896564503e60c8b5d62b111af44
kumarraja <kumarraja1989@hotmail.com>
2020-09-23 09:46:41 +0530

    freecodecamp go tutorial started

---
+++ basics/freecodecamp/variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+/*
+Agenda:
+1. Variable Declaration
+2. Redeclaration and Shadowing
+3. Visibility
+4. Naming Convensions
+5. Type Conversions
+*/
+
+func main() {
+	fmt.Println("this is variables tutorial")
+}
